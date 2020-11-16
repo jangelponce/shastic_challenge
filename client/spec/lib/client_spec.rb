c892ea7438c2069c8311dee02b226354852a1458
Angel Ponce <j.angelponce.esc@gmail.com>
2020-11-15 18:30:41 -0800

    Adding rspec for Client

---
+++ client/spec/lib/client_spec.rb
@@ -0,0 +1,11 @@
+require "initializer"
+
+describe Client do
+  describe ".visits" do 
+    it "returns array of hashes" do
+      visits = Client.visits
+      expect(visits).to be_a(Array)
+      expect(visits[0]).to be_a(Hash)
+    end
+  end
+end
